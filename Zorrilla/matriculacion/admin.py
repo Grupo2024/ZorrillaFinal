0ac25d17143d962a84e8aecaf3c43219fa375df2
MirandaGonzalo <gonzamirandab2000@gmail.com>
2018-05-13 22:52:18 -0300

    Finished the models, and the Alumno data is showed in the terminal

--- Zorrilla/matriculacion/admin.py
+++ Zorrilla/matriculacion/admin.py
@@ -3,8 +3,8 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(Persona)
 admin.site.register(Profesor)
 admin.site.register(Transportista)
 admin.site.register(Padre_madre)
 admin.site.register(Alumno)
+admin.site.register(Autorizado)
--- Zorrilla/matriculacion/models.py
+++ Zorrilla/matriculacion/models.py
@@ -38,14 +38,14 @@ class Persona(models.Model):
 
 class Profesor(Persona):
     #foto = models.ImageField(upload_to=get_image_path, blank=True, null=True)
-    telefono_particular_p = models.IntegerField(max_length=15)
-    telefono_laboral_p = models.IntegerField(max_length=15)
-    telefono_familiar_p = models.IntegerField(max_length=15)
-    datos_familiares_cargo = models.TextArea(max_length=300)
+    telefono_particular_p = models.IntegerField()
+    telefono_laboral_p = models.IntegerField()
+    telefono_familiar_p = models.IntegerField()
+    datos_familiares_cargo = models.TextField(max_length=300)
     fecha_inicio_actividad = models.DateTimeField()
-    antecedentes_laborales = models.TextArea(max_length=300)
+    antecedentes_laborales = models.TextField(max_length=300)
     antiguedad_en_empresa = models.DateTimeField()
-    estudios_cursados = models.TextArea(max_length=300)
+    estudios_cursados = models.TextField(max_length=300)
 
     def __str__(self):
         return 'Persona: {} {}| dni: {}| sexo: {}'.format(self.nombre, self.apellido, self.dni, self.sexo)
@@ -53,6 +53,7 @@ class Profesor(Persona):
 
 
 class Autorizado(Persona):
+    autorizacion = models.BooleanField()
 
     def __str__(self):
         return 'Persona: {} {}| dni: {}|'.format(self.nombre, self.apellido, self.dni)
@@ -61,8 +62,8 @@ class Autorizado(Persona):
 
 class Transportista(Persona):
     nombre_transporte = models.CharField(max_length=40)
-    telefono_transportista = models.IntegerField(max_length=15)
-    detalles_transportista = models.TextArea(max_length=300)
+    telefono_transportista = models.IntegerField()
+    detalles_transportista = models.TextField(max_length=300)
 
     def __str__(self):
         return 'Persona: {} {}| dni: {}|'.format(self.nombre, self.apellido, self.dni)
@@ -72,7 +73,7 @@ class Transportista(Persona):
 class Padre_madre(Persona):
     cant_hijos_en_colegio = models.IntegerField(null=False)
     profesion = models.CharField(max_length=40)
-    telefono_trabajo = models.IntegerField(max_length=15)
+    telefono_trabajo = models.IntegerField()
 
     def __str__(self):
         return 'Persona: {} {}| dni: {}|'.format(self.nombre, self.apellido, self.dni)
@@ -80,27 +81,23 @@ class Padre_madre(Persona):
 
 
 class Alumno(Persona):
-    telefono_casa = models.IntegerField(max_length=15)
-    telefono_padre = models.IntegerField(max_length=15)
-    telefono_madre = models.IntegerField(max_length=15)
-    telefono_familiar = models.IntegerField(max_length=15)
-    telefono_vecino = models.IntegerField(max_length=15)
+    telefono_casa = models.IntegerField()
+    telefono_padre = models.IntegerField()
+    telefono_madre = models.IntegerField()
+    telefono_familiar = models.IntegerField()
+    telefono_vecino = models.IntegerField()
     enfermedad_relevante = models.CharField(max_length=40)
     con_quien_vive = models.CharField(max_length=40)
     quien_lo_trae = models.CharField(max_length=40)
-    telefono_que_lo_trae = models.IntegerField(max_length=15)
-    quien_lo_retira = models.ForeignKey(Autorizado) #Quien esta autorizado a retirarlo
-    utiliza_transporte = models.BooleanField(null = True) #Si viene o se va en transporte
-    transporte = models.ForeignkKey(Transportista)
+    telefono_que_lo_trae = models.IntegerField()
+    utiliza_transporte = models.BooleanField() #Si viene o se va en transporte
+    transporte = models.ForeignKey(Transportista)
     autorizados = models.ForeignKey(Autorizado)
     padres = models.ForeignKey(Padre_madre)
     tiene_obra_social = models.BooleanField()
     obra_social_nombre = models.CharField(max_length=40, null=True)
-    obra_social_numero = models.IntegerField(max_length=30, null=True) #Numero de afiliacion a la ora social
+    obra_social_numero = models.IntegerField() #Numero de afiliacion a la ora social
 
 
     def __str__(self):
         return 'Persona: {} {}| dni: {}| sexo: {}'.format(self.nombre, self.apellido, self.dni, self.sexo)
-
-
-
