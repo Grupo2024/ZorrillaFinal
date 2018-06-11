c56b2f7d8739e20a5a8e1e9e95338413548207dc
MirandaGonzalo <gonzamirandab2000gmail.com>
2018-06-11 19:16:19 -0300

    Forms.py is working

--- Zorrilla/biblioteca/views.py
+++ Zorrilla/biblioteca/views.py
@@ -8,33 +8,11 @@ def filter_books(request):
     return render(request, 'filter_books.html')
 
 def biblioteca(request):
-    documents = Document.objects.all().order_by('?')[:2]
+    documents = Document.objects.all().order_by('?')[:5]
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
-        description=request.POST['description']
-        document=request.FILES['document']
-        title=request.POST['title']
-        tipo=request.POST['tipo']
-        autor=request.POST['autor']
-        editorial=request.POST['editorial']
-        fecha_lanzamiento=request.POST['fecha_lanzamiento']
-        cantidad_paginas=request.POST['cantidad_paginas']
-        genero=request.POST['genero']
-        habilitado=request.POST['habilitado']
-        if habilitado == 'on':
-            habilitado = True
-        else:
-            habilitado = False
-        document = Document (description=description, document=document, title=title, tipo=tipo,
-        autor=autor,editorial=editorial, fecha_lanzamiento=fecha_lanzamiento, cantidad_paginas=cantidad_paginas
-        , genero=genero, habilitado=habilitado)
-
-        document.save()
-
-        print document.document.name
-
-        estado = Estado(document=document, user=request.user, modificacion="Crear")
-        estado.save()
+        if form.is_valid():
+            form.save()
         return redirect ('biblioteca')
 
     else:
