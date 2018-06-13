ee18584295bc88714978625f01f8eff19b307f62
MirandaGonzalo <gonzamirandab2000gmail.com>
2018-06-13 12:54:40 -0300

    Done

--- Zorrilla/matriculacion/urls.py
+++ Zorrilla/matriculacion/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from .views import *
 from django.conf import settings
-from django.conf.urls.static import static 
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', index, name="index"),
@@ -12,5 +12,5 @@ urlpatterns = [
     url(r'^pedidos', aceptar_matriculaciones, name="pedidos"),
     url(r'crear_alumno/', crear_alumno, name="crear_alumno"),
     url(r'^aceptar_matriculacion/(?P<id_matriculacion>\d+)$', aceptar_matriculacion, name="aceptar_matriculacion"),
-    url(r'^perfilAlumno/(\id_alumno+)/$', alumno, name="alumno"),
+    url(r'^perfilAlumno/(\d+)/$', alumno, name="alumno"),
 ]
--- Zorrilla/matriculacion/views.py
+++ Zorrilla/matriculacion/views.py
@@ -17,14 +17,11 @@ def cursos(request):
     return render(request, 'cursos.html')
 
 def logIn(request):
-    return render(request, 'docentes_login.html') 
+    return render(request, 'docentes_login.html')
 
 def aceptar_matriculaciones(request):
-    print request.user
     alumnos = Alumno.objects.all()
-    print alumnos
     matriculaciones = Matriculacion.objects.filter(matriculado=False)
-    print matriculaciones
     return render(request, 'pedidos.html', {'matriculaciones':matriculaciones})
 
 def aceptar_matriculacion(request, id_matriculacion):
@@ -45,5 +42,4 @@ def aceptar_matriculacion(request, id_matriculacion):
 
 def alumno(request, id_alumno):
     alumno = Alumno.objects.get(dni=id_alumno)
-    print alumno
-    return render(request, 'templates_cursos/perfilAlumno.html', {'alumno':alumno})
+    return render(request, 'perfilAlumno.html', {'alumno':alumno})
