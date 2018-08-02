efe8cca24e36a70c022f7e4a46ae05e5b6337998
MirandaGonzalo <gonzamirandab2000gmail.com>
2018-08-02 10:39:03 -0300

    Now the Director cant delete books

--- Zorrilla/biblioteca/templatetags/poll_extras.py
+++ Zorrilla/biblioteca/templatetags/poll_extras.py
@@ -5,13 +5,5 @@ register = template.Library()
 
 @register.filter(name='has_group') 
 def has_group(user, group_name):
-    group =  Group.objects.get(name=group_name) 
+    group = Group.objects.get(name=group_name) 
     return group in user.groups.all() 
-
-"""
-{% if request.user|has_group:"Profesor" %} 
-    <p>User belongs to my group 
-{% else %}
-    <p>User doesn't belong to mygroup</p>
-{% endif %}
-"""
